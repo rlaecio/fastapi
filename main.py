08c67abee857deb420c349c84492171d988acd3c
Roque Souza <rlaecio@hotmail.com>
2023-03-09 17:12:08 +0000

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI,  Depends, Request
 from sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
+
 from starlette.responses import RedirectResponse
 from model import Registo
 import model
@@ -13,6 +14,9 @@ import model
 model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 def get_database_session():
     try:
@@ -27,16 +31,18 @@ def get_database_session():
 def read_root():
     return {"message": "welcome to FastAPI!"}
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
 @app.get("/registo")
+async def lis_item(db: Session = Depends(get_database_session)):
+    registos = db.query(Registo).all()
+    return  registos 
+    
+
+@app.get("/principal", response_class=HTMLResponse)
 async def lis_item(request: Request, db: Session = Depends(get_database_session)):
-    records = db.query(Registo).all()
+    registos = db.query(Registo).all()
     #return  records 
-    return templates.TemplateResponse("index.html", {"request": request, "data": records})
-
-
+    return templates.TemplateResponse("principal.html", {"request": request, "data": registos })
 
 
 
